b1e8ffc3ad6168445d27883d3cb0383aa8c8e13e
SageSG <houliangyip@gmail.com>
2017-09-21 12:14:44 +0800

    url

--- nodemodjs/config/databaseConfig.js
+++ nodemodjs/config/databaseConfig.js
@@ -1,6 +1,6 @@
 var databaseConfig = {}
 
-databaseConfig.url = 'http://ba96b893.ngrok.io'
+databaseConfig.url = 'http://ba96b893.ngrok.io/api'
 
 databaseConfig.primary_key = 'NnGUnatosykldCDs6m5Ma4tBGlb6Wyue912JLQ=='
 
