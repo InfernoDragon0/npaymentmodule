102d73f61b0dbe889304b1727f66a58b30b0f2c6
Unknown <calebcheongdnt@gmail.com>
2017-09-26 12:20:54 +0800

    Auto stash before merge of "master" and "origin/master"

--- nodemodjs/config/databaseConfig.js
+++ nodemodjs/config/databaseConfig.js
@@ -5,6 +5,6 @@ databaseConfig.url = 'http://becfec34.ngrok.io/api'
 databaseConfig.primary_key = 'NnGUnatosykldCDs6m5Ma4tBGlb6Wyue912JLQ=='
 
 process.env.PRIMARY_KEY = 'NnGUnatosykldCDs6m5Ma4tBGlb6Wyue912JLQ=='
-process.env.URL = 'http://8b7409f6.ngrok.io/api'
+process.env.URL = 'http://d7da5558.ngrok.io/api'
 
 module.exports = databaseConfig
