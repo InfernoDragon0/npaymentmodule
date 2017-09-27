4731c680645b22a24a44e3bd3653884b28e5d0b0
Unknown <calebcheongdnt@gmail.com>
2017-09-27 13:11:12 +0800

    dsa

--- nodemodjs/config/databaseConfig.js
+++ nodemodjs/config/databaseConfig.js
@@ -1,10 +1,10 @@
 var databaseConfig = {}
 
-databaseConfig.url = 'http://becfec34.ngrok.io/api'
+databaseConfig.url = 'http://9919580f.ngrok.io/api'
 
 databaseConfig.primary_key = 'NnGUnatosykldCDs6m5Ma4tBGlb6Wyue912JLQ=='
 
 process.env.PRIMARY_KEY = 'NnGUnatosykldCDs6m5Ma4tBGlb6Wyue912JLQ=='
-process.env.URL = 'http://d7da5558.ngrok.io/api'
+process.env.URL = 'http://9919580f.ngrok.io/api'
 
 module.exports = databaseConfig
