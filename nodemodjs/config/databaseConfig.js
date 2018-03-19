d51467451e9b64bbe94348e0772f0226786b2045
Unknown <calebcheongdnt@gmail.com>
2018-03-19 21:59:21 +0800

    update

--- nodemodjs/config/databaseConfig.js
+++ nodemodjs/config/databaseConfig.js
@@ -1,10 +1,10 @@
 var databaseConfig = {}
 
-databaseConfig.url = 'http://9919580f.ngrok.io/api'
+databaseConfig.url = 'http://johorbahru.azurewebsites.net/api'
 
 databaseConfig.primary_key = 'NnGUnatosykldCDs6m5Ma4tBGlb6Wyue912JLQ=='
 
 process.env.PRIMARY_KEY = 'NnGUnatosykldCDs6m5Ma4tBGlb6Wyue912JLQ=='
-process.env.URL = 'http://9919580f.ngrok.io/api'
+process.env.URL = 'http://johorbahru.azurewebsites.net/api'
 
 module.exports = databaseConfig
